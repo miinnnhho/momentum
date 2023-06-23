f5cb405057b317eb6ad01c0e2e48055fc2e369c7
minho <jomin981007@naver.com>
2023-06-23 13:41:38 +0900

    start Todo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.scss";
 import Search from "./components/search/search";
 import Weather from "./components/weather/weather";
 import Nowtime from "./components/Time/Time";
+// import Greeting from "./components/greeting/greeting";
+import DragAndDropList from "./components/todo/todo";
 
 function App() {
   return (
-    <body>
-      <div>
-        <Search />
-        <Weather />
-        <Nowtime />
-      </div>
-    </body>
+    <div className="main">
+      <Search />
+      <Weather />
+      <Nowtime />
+      {/* <Greeting /> */}
+      <DragAndDropList />
+    </div>
   );
 }
 
